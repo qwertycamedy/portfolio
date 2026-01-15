fd1e0c4d6c23cdb900429a6b7e5051d5f0deb0e6
Adil Kairbekov <qwertycamedy@gmail.com>
2026-01-15 07:10:26 +0500

    feat(hero): hero section path

--- src/components/shared/_common/sidebar.tsx
+++ src/components/shared/_common/sidebar.tsx
@@ -31,57 +31,62 @@ export const Sidebar = () => {
   }, []);
 
   return (
-    <Card className={'h-max max-w-70 flex-col gap-5 p-5.5'}>
-      <img
-        className="h-19 w-19 rounded-full"
-        src={meImg}
-        alt="Adil Kairbekov qwertycamedy Frontend Developer"
-      />
-      <div className="flex flex-col gap-2">
-        <Title text="Adil Kairbekov" size={TitleSizes.h5} />
-        <p className="text-secondary-foreground text-xs">Frontend developer</p>
-      </div>
-      <ul className="flex gap-3.5">
-        {networkLinks.map((networkLink) => (
-          <li>
-            <Link
-              className="bg-primary flex h-7.5 w-7.5 items-center justify-center overflow-hidden rounded-full text-lg transition-opacity hover:opacity-60"
-              to={networkLink.path}
-              target="_blank"
-              key={networkLink.path}
-            >
-              {networkLink.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <ul className="flex flex-col gap-3 pl-3.5">
-        {navLinks.map((navLink) => (
-          <li>
-            <Link
-              className="text-secondary-foreground relative flex items-center gap-3 text-sm"
-              to={`/#${navLink.path}`}
-              key={navLink.path}
-            >
-              <span
-                className={cn(
-                  'bg-primary pointer-events-none absolute flex h-1.5 w-1.5 rounded-full opacity-0 transition-opacity',
-                  { 'opacity-100': activeNavLink === navLink.path },
-                )}
-              />
-              <span
-                className={cn(
-                  'bg-primary text-foreground flex h-5 w-5 items-center justify-center overflow-hidden rounded-full',
-                  { 'rotate-180': navLink.path === 'about' },
-                )}
+    <aside className="flex h-max w-full max-w-60 flex-col">
+      <Card className={'flex-col gap-5 px-5.5 pt-5.5 pb-12'}>
+        <img
+          className="h-19 w-19 rounded-full"
+          src={meImg}
+          alt="Adil Kairbekov qwertycamedy Frontend Developer"
+        />
+        <div className="flex flex-col gap-2">
+          <Title text="Adil Kairbekov" size={TitleSizes.h5} />
+          <p className="text-secondary-foreground text-xs">
+            Frontend developer
+          </p>
+        </div>
+        <ul className="flex gap-3.5">
+          {networkLinks.map((networkLink) => (
+            <li key={networkLink.path}>
+              <Link
+                className="bg-primary flex h-7.5 w-7.5 items-center justify-center overflow-hidden rounded-full text-lg transition-opacity hover:opacity-60"
+                to={networkLink.path}
+                target="_blank"
               >
-                <navLink.icon size={14} />
-              </span>
-              <span>{navLink.text}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Card>
+                {networkLink.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <ul className="flex flex-col gap-3 pl-3.5">
+          {navLinks.map((navLink) => (
+            <li key={navLink.path}>
+              <Link
+                className={cn(
+                  'text-secondary-foreground relative flex items-center gap-3 text-sm hover:opacity-70 transition-opacity',
+                  { 'text-foreground': activeNavLink === navLink.path },
+                )}
+                to={`/#${navLink.path}`}
+              >
+                <span
+                  className={cn(
+                    'bg-primary pointer-events-none absolute right-[calc(100%+8px)] flex h-1.5 w-1.5 rounded-full opacity-0 transition-opacity',
+                    { 'opacity-100': activeNavLink === navLink.path },
+                  )}
+                />
+                <span
+                  className={cn(
+                    'bg-primary text-foreground flex h-5 w-5 items-center justify-center overflow-hidden rounded-full',
+                    { 'rotate-180': navLink.path === 'about' },
+                  )}
+                >
+                  <navLink.icon size={14} />
+                </span>
+                <span>{navLink.text}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Card>
+    </aside>
   );
 };
--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom';
 import { Button, Card, Title, TitleSizes } from '../ui';
+import { navLinks, networkLinks } from '@/utils';
 
 export const Hero = () => {
   return (
-    <section id="hero">
-      <Card>
-        <p>Qwerty Camedy</p>
-        <Title text="Frontend Developer" size={TitleSizes.h1} />
-        <Button>Нанять меня</Button>
+    <section className="flex w-full" id={navLinks[0].path}>
+      <Card className="h-max flex-col">
+        <p className="mb-2">Qwerty Camedy</p>
+        <Title
+          className={'mb-5'}
+          text="Frontend Developer"
+          size={TitleSizes.h1}
+        />
+        <Link to={networkLinks[1].path} target="blank">
+          <Button className="w-max px-5 py-3">Нанять меня</Button>
+        </Link>
       </Card>
     </section>
   );
