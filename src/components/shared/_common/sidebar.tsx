54a07a589aa36f9c855613528331aac2749e8587
Adil Kairbekov <qwertycamedy@gmail.com>
2026-02-07 04:32:35 +0500

    feat(sidebar): responsive sidebar

--- src/components/shared/_common/sidebar.tsx
+++ src/components/shared/_common/sidebar.tsx
@@ -3,10 +3,11 @@ import { Link as ScrollLink } from 'react-scroll';
 import { Card, Title, TitleSizes } from '@/components/ui';
 import meImg from '@/assets/img/me.jpg';
 import { cn, navLinks } from '@/utils';
-import { useScrollBottomMonitor } from '@/hooks';
+import { useIsMobile, useScrollBottomMonitor } from '@/hooks';
 import { NetworkLinks } from './network_links';
 
 export const Sidebar = () => {
+  const { isMobile, isTablet } = useIsMobile();
   const isBottom = useScrollBottomMonitor(navLinks[3].path);
   const [activeNavLink, setActiveNavLink] = useState<string>(navLinks[0].path);
 
@@ -15,21 +16,34 @@ export const Sidebar = () => {
   };
 
   return (
-    <aside className="sticky top-5 flex h-max w-60 min-w-60 flex-col">
-      <Card className={'flex-col gap-5 px-5.5 pt-5.5 pb-12'}>
-        <img
-          className="h-19 w-19 rounded-full"
-          src={meImg}
-          alt="Adil Kairbekov qwertycamedy Frontend Developer"
-        />
-        <div className="flex flex-col gap-2">
-          <Title text="Adil Kairbekov" size={TitleSizes.h5} />
-          <p className="text-secondary-foreground text-xs">
-            Frontend developer
-          </p>
-        </div>
-        <NetworkLinks />
-        <ul className="flex flex-col gap-3 pl-3.5">
+    <aside
+      className={cn(
+        'flex flex-col lg:sticky lg:top-5 lg:bottom-[unset] lg:left-[unset] lg:h-max lg:w-60 lg:min-w-60 lg:bg-transparent lg:pb-0',
+        'fixed bottom-0 left-0 z-1000 w-full pb-5',
+      )}
+    >
+      <Card
+        className={cn(
+          'mx-auto w-max flex-col gap-5 rounded-full px-8 pt-5 pb-5 shadow-2xl backdrop-blur-lg md:rounded-full md:px-8 md:pt-5 md:pb-5 lg:mx-0 lg:w-auto lg:px-5.5 lg:pt-5.5 lg:pb-12 lg:shadow-none lg:backdrop-blur-none',
+        )}
+      >
+        {!isMobile && !isTablet && (
+          <>
+            <img
+              className="h-19 w-19 rounded-full"
+              src={meImg}
+              alt="Adil Kairbekov qwertycamedy Frontend Developer"
+            />
+            <div className="flex flex-col gap-2">
+              <Title text="Adil Kairbekov" size={TitleSizes.h5} />
+              <p className="text-secondary-foreground text-xs">
+                Frontend developer
+              </p>
+            </div>
+            <NetworkLinks />
+          </>
+        )}
+        <ul className="mx-auto flex gap-5 pl-0 lg:mx-0 lg:flex-col lg:gap-3 lg:pl-3.5">
           {navLinks.map((navLink) => (
             <li key={navLink.path}>
               <ScrollLink
@@ -50,9 +64,9 @@ export const Sidebar = () => {
               >
                 <span
                   className={cn(
-                    'bg-primary pointer-events-none absolute right-[calc(100%+8px)] flex h-1.5 w-1.5 rounded-full opacity-0 transition-opacity',
+                    'bg-primary pointer-events-none absolute top-[calc(100%-4px)] left-1/2 flex h-1.5 w-1.5 -translate-1/2 rounded-full opacity-0 transition-all duration-500 lg:top-[unset] lg:right-[calc(100%+8px)] lg:left-[unset] lg:translate-0 lg:transition-opacity lg:duration-200',
                     {
-                      'opacity-100':
+                      'top-[calc(100%+4px)] opacity-100':
                         (!isBottom && activeNavLink === navLink.path) ||
                         (isBottom && navLink.path === navLinks[3].path),
                     },
@@ -60,13 +74,18 @@ export const Sidebar = () => {
                 />
                 <span
                   className={cn(
-                    'bg-primary text-foreground flex h-5 w-5 items-center justify-center overflow-hidden rounded-full',
-                    { 'rotate-180': navLink.path === 'about' },
+                    'bg-primary text-foreground flex h-10 w-10 items-center justify-center overflow-hidden rounded-full opacity-70 transition-all lg:h-5 lg:w-5 lg:translate-y-0 lg:opacity-100 lg:transition-none',
+                    {
+                      'rotate-180': navLink.path === 'about',
+                      '-translate-y-1 opacity-100':
+                        (!isBottom && activeNavLink === navLink.path) ||
+                        (isBottom && navLink.path === navLinks[3].path),
+                    },
                   )}
                 >
                   <navLink.icon size={14} />
                 </span>
-                <span>{navLink.text}</span>
+                {!isMobile && !isTablet && <span>{navLink.text}</span>}
               </ScrollLink>
             </li>
           ))}
--- src/components/ui/page.tsx
+++ src/components/ui/page.tsx
@@ -1,13 +1,12 @@
 import { cn } from '@/utils';
 import { ReactNode } from 'react';
 import { ClassNameValue } from 'tailwind-merge';
-import { Burger, BurgerMenu, Footer, Sidebar } from '@/components/shared';
+import { Footer, Sidebar } from '@/components/shared';
 import { QcIcoOutline } from '@/components/icons';
-import { useIsMobile } from '@/hooks';
 
 export const Page = ({
   sidebar = true,
-  burger = true,
+  // burger = true,
   footer = true,
   logo = true,
   className,
@@ -15,33 +14,39 @@ export const Page = ({
   children,
 }: {
   sidebar?: boolean;
-  burger?: boolean;
+  // burger?: boolean;
   footer?: boolean;
   logo?: boolean;
   className?: ClassNameValue;
   contentCl?: ClassNameValue;
   children: ReactNode;
 }) => {
-  const { isMobile, isTablet } = useIsMobile();
+  // const { isMobile, isTablet } = useIsMobile();
 
   return (
-    <div className={cn('flex min-h-svh flex-1 flex-col gap-5 p-5', className)}>
+    <div
+      className={cn(
+        'flex min-h-svh flex-1 flex-col gap-5 px-5 pt-5 pb-30 lg:p-5',
+        className,
+      )}
+    >
       <div
         className={cn('mx-auto flex w-full max-w-7xl flex-1 gap-5', contentCl)}
       >
-        {!isMobile && !isTablet && sidebar && <Sidebar />}
+        {/* {!isMobile && !isTablet && sidebar && <Sidebar />} */}
+        {sidebar && <Sidebar />}
         {children}
       </div>
-      {(isMobile || isTablet) && burger && (
+      {/* {(isMobile || isTablet) && burger && (
         <>
           <Burger />
           <BurgerMenu />
         </>
-      )}
+      )} */}
       {footer && <Footer />}
       {logo && (
         <QcIcoOutline
-          className="text-primary/5 pointer-events-none fixed bottom-40 left-[calc(60%+100px)] -z-1 h-[27vw] w-[27vw]"
+          className="text-primary/5 pointer-events-none fixed bottom-40 left-[calc(40%+100px)] lg:left-[calc(60%+100px)] -z-1 h-[40vh] w-[40vh]  md:h-[27vw] md:w-[27vw]"
           size={400}
         />
       )}
